5265a7508a1e0de7efaa9987abe03d7c3e33c3aa
Tyko <tykovv@gmail.com>
2022-10-22 19:34:01 -0600

    removed other stuff

--- zig-stuff/ppu.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-
-pub fn main() !void {
-    _ = try std.os.pipe();
-    const null_list: [:null]const ?[*:0]const u8 = &[:null]?[*:0]const u8{null};
-    _ = std.os.execveZ("ls", null_list, null_list);
-}
