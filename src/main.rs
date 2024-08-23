a8d4183fffbaa45bb79181f39cdc94aa813b7bda
Tyko <tykovv@gmail.com>
2024-08-23 16:53:07 -0600

    Unsure what this was lol

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,30 @@
-// use minifb::{Window, WindowOptions};
-use pixels::{Pixels, SurfaceTexture};
-use winit::dpi::LogicalSize;
-use winit::event_loop::EventLoop;
-use winit::window::WindowBuilder;
+use minifb::{Key, Window, WindowOptions};
 
-const WIDTH: u32 = 240;
-const HEIGHT: u32 = 160;
-
-struct DisplayPixels {
-    pixels: Pixels,
-}
-
-impl DisplayPixels {
-    fn new() -> Self {
-        let event_loop = EventLoop::new();
-        // let window = WindowBuilder::new().build(&event_loop).unwrap();
-        let builder = WindowBuilder::new();
-        let builder = builder.with_inner_size(LogicalSize {
-            width: WIDTH,
-            height: HEIGHT,
-        });
-        let builder = builder.with_resizable(false);
-        let window = builder.build(&event_loop).unwrap();
-
-        let size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(size.width, size.height, &window);
-        let pixels = Pixels::new(240, 160, surface_texture).unwrap();
-        Self { pixels }
-    }
-
-    fn clear(&mut self) {
-        let frame = self.pixels.get_frame_mut();
-        for pixel in frame.chunks_exact_mut(4) {
-            pixel[0] = 0x00; // R
-            pixel[1] = 0x00; // G
-            pixel[2] = 0x00; // B
-            pixel[3] = 0xff; // A
-        }
-        self.pixels.render().unwrap();
-    }
-}
-
-// struct DisplayMinifb {
-//     window: Window,
-// }
-
-// impl DisplayMinifb {
-//     fn new() -> Self {
-//         let mut window = Window::new("Display", WIDTH, HEIGHT, WindowOptions::default()).unwrap();
-//         window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
-//         Self { window }
-//     }
-// }
+const WIDTH: usize = 240;
+const HEIGHT: usize = 160;
 
 fn main() {
-    let mut display = DisplayPixels::new();
-    display.clear();
-    // let mut display = DisplayMinifb::new();
-    println!("Hello, world!");
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = 0; // write something more funny here!
+        }
+
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+    }
 }
